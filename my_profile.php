4f78a5e8514a997689766a3470b199cd085bb0e6
kazuya549 <ito.kazuya549@gmail.com>
2025-07-09 01:49:01 +0900

    add:my_profile.php

---
+++ my_profile.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=320, height=480, initial-scale=1.0, minimum-scale=1.0, maximum-scale=2.0, user-scalable=yes">
+    <meta charset="utf-8">
+    <title>伊藤和弥のプロフィール</title>
+    <style>
+        body {
+            text-align: center; /* 全体中央寄せ */
+        }
+        img {
+            width: 300px;
+            border-radius: 30%;
+            display: block;
+            margin: 20px auto; /* 画像中央 */
+        }
+        ol, ul {
+            list-style-position: inside; /* 番号や点含め中央寄せ */
+            text-align: center;
+            padding-left: 0;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+
+<h1 id="midashi_1">MY Profile 伊藤和弥</h1>
+
+<img src="egyptian_pyramid.jpg" alt="エジプトでの写真" title="プロフィール画像" width="360px" height="240px">
+（3月に大学の卒業旅行 in Egypt）<br>
+
+<hr>
+
+<h2>所属</h2>
+<ul>
+    <li>福岡の理系大学の大学院1年生です。</li>
+    <li>大学院では、Auto-mobility分野の研究をしています。</li>
+    <li>アルバイトは飛行機が綺麗に見えるカフェでバリスタをしています。</li>
+</ul>
+
+<h2>趣味や属性</h2>
+<ul>
+    <li>海外一人旅が好きです。(カナダ、メキシコ、韓国 etc.)</li>
+    <li>友達と過ごす時間も好きですが、今は大学の研究に追われています笑</li>
+</ul>
+
+<h2>ランキング</h2>
+<strong>行ってみたい国TOP3</strong><br>
+<ol>
+    <li>トルコ</li>
+    <li>ウズベキスタン</li>
+    <li>ブラジル</li>
+</ol>
+
+<strong>行ってよかった場所TOP3</strong><br>
+<ul>
+    <li>地中海都市アレクサンドリア <a href="https://ja.wikipedia.org/wiki/アレクサンドリア" target="_blank">アレクサンドリアへ飛ぶ</a></li>
+    <li>バンクーバーの English Bay Beach <a href="https://ja.wikipedia.org/wiki/バンクーバー_(ブリティッシュコロンビア州)" target="_blank">EBBへ飛ぶ</a></li>
+    <li>沖縄のフクギ並木 <a href="https://fukugi-namiki.com" target="_blank">フクギ並木へ飛ぶ</a></li>
+</ul>
+
+<hr>
+
+<h2>その他</h2>
+
+<h3>使用PC</h3>
+Windows(大学の研究室)<br>
+Mac(自宅)<br>
+
+<h3>PC利用経験や普段の使い方</h3>
+PCに関しては大学で毎日使っていますが、研究ではPythonを使っているのでHTMLに関しては触るのは約3年ぶりです。
+
+<h3>自分の強みや弱みなど</h3>
+強み：行動力があります。コミュニケーション力は日々のアルバイトで鍛えられていると思います。<br>
+弱み：「追い詰められた自分の集中力はすごい」ということを計画に入れてしまうこと<br>
+
+<h3>TECH-BASE参加にあたり、意気込み</h3>
+皆さん最後まで共に頑張りましょう！！<br>
+
+<hr>
+
+<a href="#midashi_1">このページのTOPへ</a> ／ <a href="https://www.google.co.jp/" target="_blank">ググる</a>
+
+<hr>
+
+<form method="POST" action="">
+    お名前：<input type="text" name="name"><br>
+    コメント：<input type="text" name="comment"><br>
+    <input type="submit" name="submit" value="送信">
+</form>
+
+</body>
+</html>
